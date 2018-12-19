3e821de30f417b9627c1666108de6f116f6e9591
gaurav <sharma02gaurav@gmail.com>
2018-12-19 23:12:45 +0530

    example service

---
+++ src/service.js
@@ -0,0 +1,10 @@
+/**
+ * @description This is the default service that will be doing something and will
+ * eventually be imported in index.js and hence exported. Fell free to play arounf
+ * and happy coding.
+ * @author gaurav sharma
+ * @since 19th December 2018
+ */
+export default () => {
+	return "Plugin Starter Kit.";
+};
